8712d05a2ec0de85c5684dd9124170d1925a0596
Fredrik Haglund <mejl@fredrikhaglund.se>
2018-10-03 09:02:53 +0200

    Add project files.

---
+++ Exceptions/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static int GoBoom(int a, int b)
+        {
+            var result = a / b;
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine(GoBoom(100, 0));
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Något gick fel!");
+                throw;
+            }
+
+
+            var x = int.Parse("XYZ");
+
+            var success = int.TryParse("XYZ", out int value);
+            if (success) // success == true
+            {
+                // Använd value till något
+            }
+            else
+            {
+                // Egen felhantering - t ex fråga igen.
+            }
+
+            int age = CalculteAgeInDays(new DateTime(2019, 10, 3));
+            Console.WriteLine("Age in days: " + age);
+
+            DoStuff();
+        }
+
+        private static int CalculteAgeInDays(DateTime birthDate)
+        {
+            if (birthDate > DateTime.Now)
+                throw new ArgumentOutOfRangeException( nameof(birthDate), "Can not be a date in the future");
+            return (DateTime.Now - birthDate).Days;
+        }
+
+        private static bool TryCalculteAgeInDays(DateTime birthDate, out int age)
+        {
+            var success = (birthDate < DateTime.Now);
+            age = (DateTime.Now - birthDate).Days;
+            return success;
+        }
+
+        private static void DoStuff()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NestedExceptionsHandlers/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedExceptionsHandlers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Enter Main");
+
+            Console.WriteLine("Call MethodA()");
+            MethodA();
+
+            //Console.WriteLine("Call MethodC()");
+            //MethodC(0);
+
+            Console.WriteLine($"Exit Main");
+        }
+
+        static void MethodA()
+        {
+            Console.WriteLine($"Enter MethodA");
+            try
+            {
+                Console.WriteLine($"Enter try");
+                MethodB();
+                Console.WriteLine($"Exit try");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine($"Enter catch");
+                Console.WriteLine("Division med noll - skriv in en annan täljare!");
+                //throw;
+                Console.WriteLine($"Exit catch");
+            }
+            finally
+            {
+                Console.WriteLine($"Enter finally");
+                Console.WriteLine("Städa - t ex stänga en fil");
+                Console.WriteLine($"Exit finally");
+            }
+            Console.WriteLine($"Exit MethodA");
+        }
+
+        static void MethodB()
+        {
+            Console.WriteLine($"Enter MethodB");
+            MethodC(0);
+            Console.WriteLine($"Exit MethodB");
+        }
+
+        static void MethodC(int x)
+        {
+            Console.WriteLine($"Enter MethodC");
+            MethodD(10, x);
+            Console.WriteLine($"Exit MethodC");
+        }
+
+        static int MethodD(int a, int b)
+        {
+            Console.WriteLine($"Enter MethodD");
+
+            // Do stuff
+            var result = a / b;
+
+            Console.WriteLine($"Before Return in MethodD");
+            return result;          
+        }
+    }
+}
